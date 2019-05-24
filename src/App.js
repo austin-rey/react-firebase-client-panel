5642a32e7333b090f864fa03a0a87c9d4953feae
austin-rey <austinrey95@gmail.com>
2019-05-24 16:09:00 -0500

    Started defining the flow of the application and how components interact with each other

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
+import Dashboard from "./components/layout/Dashboard";
 import AppNavbar from "./components/layout/AppNavbar";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <AppNavbar />
         <div className="container">
-          <h1>Hello</h1>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+          </Switch>
         </div>
       </Router>
     </div>
