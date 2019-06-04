ba3a0c9e7fa470369ca9e06bef666476f2275c2b
austin-rey <austinrey95@gmail.com>
2019-06-04 16:31:10 -0500

    Defined auth helper and protected routes via redux-auth-wrapper

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { UserIsAuthenticated, UserIsNotAuthenticated } from "./helpers/auth";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -21,11 +23,31 @@ function App() {
           <AppNavbar />
           <div className="container">
             <Switch>
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/" component={Dashboard} />
-              <Route exact path="/client/add" component={AddClient} />
-              <Route exact path="/client/:id" component={ClientDetails} />
-              <Route exact path="/client/edit/:id" component={EditClient} />
+              <Route
+                exact
+                path="/"
+                component={UserIsAuthenticated(Dashboard)}
+              />
+              <Route
+                exact
+                path="/client/add"
+                component={UserIsAuthenticated(AddClient)}
+              />
+              <Route
+                exact
+                path="/client/:id"
+                component={UserIsAuthenticated(ClientDetails)}
+              />
+              <Route
+                exact
+                path="/client/edit/:id"
+                component={UserIsAuthenticated(EditClient)}
+              />
+              <Route
+                exact
+                path="/login"
+                component={UserIsNotAuthenticated(Login)}
+              />
             </Switch>
           </div>
         </Router>
